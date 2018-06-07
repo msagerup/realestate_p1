409a15342f6a51d921aa64abbad3f8fb150765b5
Magnus Sagerup <magnus.sagerup@gmail.com>
2018-06-07 22:37:44 +0200

    work on listings

--- src/components/main/Listings.js
+++ src/components/main/Listings.js
@@ -25,10 +25,41 @@ export default class Listings extends Component {
             <div className="listing-img">
               <span className="addresse">Addresse</span>
               <div className="details">
-                <div />
+                <div className="user-img" />
+                <div className="user-details">
+                  <span className="user-name">Nina Smith</span>
+                  <span className="post-date">05/05/2018</span>
+                </div>
+                <div className="listing-details">
+                  <div className="floor-space">
+                    <i class="far fa-square" />
+                    <span>100 m&sup2;</span>
+                    <div className="bedrooms">
+                      <i class="fas fa-bed" />
+                      <span>3 Soverom</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="bottom-info">
+                  <span>6000 Kr / mnd</span>
+                  <span>
+                    <i class="fas fa-map-marker" />Bislet, Oslo
+                  </span>
+                </div>
               </div>
+              <div className="details" />
             </div>
           </div>
+        </section>
+        <section className="pagination">
+          <ul className="pagination-nums">
+            <li>Prev</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>Next</li>
+          </ul>
         </section>
       </section>
     );
